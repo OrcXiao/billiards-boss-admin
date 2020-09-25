cd8a8f994d882088d6e1519dfd330f77b8802668
OrcXiao <471951913@qq.com>
2020-09-25 12:00:02 +0800

    0927,xiao, 开发签表功能

--- src/axios/index.js
+++ src/axios/index.js
@@ -3,11 +3,12 @@ import {Message} from "element-ui";
 //请求超时
 axios.defaults.timeout = 50000;
 
-// axios.defaults.baseURL = 'http://xbp77v.natappfree.cc/';
-// axios.defaults.baseWeb = 'ws://server.natappfree.cc:43332/ws';
 
-axios.defaults.baseURL = 'http://billiards.aafu.xyz:6017/';
-axios.defaults.baseWeb = 'wss://billiards.aafu.xyz:6013/ws';
+// axios.defaults.baseURL = 'http://billiards.aafu.xyz:6017/';
+// axios.defaults.baseWeb = 'wss://billiards.aafu.xyz:6013/ws';
+
+axios.defaults.baseURL = 'http://www.taiqiuplus.com:6017';
+axios.defaults.baseWeb = 'wss://www.taiqiuplus.com/ws';
 
 axios.interceptors.response.use(res => {
   //错误码为-1, 统一进行提示.
